a5f6c151a81c31adef9a008c75fa39499d9270e3
Tesseract22 <caleb2222@qq.com>
2023-06-03 12:48:11 +0800

    stadnardized build

--- Makefile
+++ Makefile
@@ -1,18 +1,13 @@
 CXX=clang++
 CXX_FLAG=-I includes
-WASM= -Wl,--no-entry -Wl,--export-all --target=wasm32 -fno-builtin --no-standard-libraries 
-
-demos := basic
+WASM= -Wl,--no-entry -Wl,--export-all --target=wasm32 -fno-builtin --no-standard-libraries
+file=make
+build: gen
+	make -f ${file}
 
 gen: src/* gen.sh
-	./gen.sh "${CXX}" "${CXX_FLAG}" "${WASM}"
+	./gen.sh "${CXX}" "${CXX_FLAG}" "${WASM}" "${file}"
 
-all: basic.wasm test
-
-basic.wasm: src/basic.cc Makefile includes/*
-	${CXX} ${CXX_FLAG} ${WASM}  src/basic.cc	 -o $@ 
-
-color.wasm: src/color.cc Makefile includes/*
 
 
 
--- gen.sh
+++ gen.sh
@@ -2,14 +2,27 @@
 CXX=$1
 CXX_FLAG=$2
 WASM=$3
-
-echo -n "" > file
+file=$4
+echo -n "" > ${file}
+echo -n "all: " >> ${file}
+# echo -e -n "\t" >> ${file}
 for entry in src/*; do
     name=${entry/\//}
     name=${name/src/}
     name=${name/.cc/}
+    wasm=index/${name}.wasm
 
-    wasm=${name}.wasm
-    echo ${wasm}: ${entry} "includes/*" >> file
-    echo -e '\t'${CXX} ${CXX_FLAG} ${WASM} ${entry} "-o" ${wasm} >> file
+    echo -n ${wasm} " " >> ${file}
+done
+echo "" >> ${file}
+for entry in src/*; do
+    name=${entry/\//}
+    name=${name/src/}
+    name=${name/.cc/}
+    wasm=index/${name}.wasm
+
+    echo ${wasm}: ${entry} "includes/*" >> ${file}
+    echo -e '\t'${CXX} ${CXX_FLAG} ${WASM} ${entry} "-o" ${wasm} >> ${file}
+
+    
 done
