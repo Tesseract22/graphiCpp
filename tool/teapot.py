d19a7cd104219dfd92bbadb9eb18b1b6cfaaaf54
Tesseract22 <caleb2222@qq.com>
2023-06-25 18:10:36 +0800

    commit
    committt

---
+++ tool/teapot.py
@@ -0,0 +1,35 @@
+
+with open("tool/data", "r") as f:
+    lines = f.readlines()
+with open("tool/teapot.cc", "w") as f:
+    firstf = True
+    firsfv = True
+    f.write("float vs[][3] = {")
+    vlen = 0
+    flen = 0
+    for l in lines:
+        arr = l.split()
+        if len(arr) != 4:
+            continue
+        if arr[0] == 'f':
+            # print("is triangle")
+            flen += 1
+            if firstf:
+                f.write("};\n")
+                print("vertex end")
+                firstf = False
+                f.write("int fs[][3] = {")
+            f.write("{%s, %s, %s},\n"%(arr[1], arr[2], arr[3]))
+            pass
+        elif arr[0] == 'v':
+            # print("is vertex")
+            vlen += 1
+            f.write("{%s, -%s, %s},\n"%(arr[1], arr[2], arr[3]))
+            pass
+        else:
+            print("unknown format")
+        
+        
+    f.write("};\n")
+    f.write("int vlen = %i;\n"%vlen)
+    f.write("int flen = %i;\n"%flen)
