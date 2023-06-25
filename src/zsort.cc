dbc19b8458cc4a62ebb5cc57cf34c97298bd2e5f
Tesseract22 <caleb2222@qq.com>
2023-06-25 18:07:52 +0800

    teapot in progress

--- src/zsort.cc
+++ src/zsort.cc
@@ -12,8 +12,8 @@ const int H = 500;
 const int W = 500;
 Canvas<H, W> cv;
 int buf[W * H];
-const int cz = -4;
-const float POV = 120;
+const int cz = -5;
+const float POV = 60;
 const float speed = 0.01;
 
 extern "C" void putInt(int x);
@@ -28,49 +28,56 @@ using namespace gcmath;
 extern "C" {
 void render(int dt) {
   Vec3D rotations = {0, dt * speed, 0};
-  static Camera camera{{0, 0, cz}, POV, {0, 0, 0}};
+  static Camera camera{{0, -2, cz}, POV, {0, 0, 0}};
   cv.fill(0xff000000);
-  for (int i = 0; i < 1; ++i) {
+  for (int i = 0; i < vlen; ++i) {
     Vec3D p = Vec3D::rotateAll({vs[i][0], vs[i][1], vs[i][2]}, rotations);
-    LOG("p: " << p)
     Vec3D cm_p = camera.projectOnCanvas(p, cv);
-    putInt(1111111111);
-    putFloat(p.x);
-    putFloat(p.y);
-    putFloat(p.z);
-    putInt(222222222);
-    putFloat(vs[i][0]);
-    putFloat(vs[i][1]);
-    putFloat(vs[i][2]);
     // putInt(1111111111);
-    cv.drawCircle(cm_p.x, cm_p.y, 5, CONST_PICKER(0xffff007f));
+    // cv.drawCircle(cm_p.x, cm_p.y, 5, CONST_PICKER(0xffff007f));
     cv.drawPixel(cm_p.x, cm_p.y, 0xffff007f);
   }
-  //   for (int i = 0; i < flen; ++i) {
-  //     Vec3D cm_1 = camera.projectOnCanvas(
-  //         Vec3D::rotateAll({vs[fs[i][0]][0], vs[fs[i][0]][1],
-  //         vs[fs[i][0]][2]},
-  //                          rotations),
-  //         cv);
-  //     Vec3D cm_2 = camera.projectOnCanvas(
-  //         Vec3D::rotateAll({vs[fs[i][1]][0], vs[fs[i][1]][1],
-  //         vs[fs[i][1]][2]},
-  //                          rotations),
-  //         cv);
-  //     Vec3D cm_3 = camera.projectOnCanvas(
-  //         Vec3D::rotateAll({vs[fs[i][2]][0], vs[fs[i][2]][1],
-  //         vs[fs[i][2]][2]},
-  //                          rotations),
-  //         cv);
+  for (int i = 0; i < 20; ++i) {
+    Vec3D cm_1 = camera.projectOnCanvas(
+        Vec3D::rotateAll({vs[fs[i][0]][0], vs[fs[i][0]][1], vs[fs[i][0]][2]},
+                         rotations),
+        cv);
+    Vec3D cm_2 = camera.projectOnCanvas(
+        Vec3D::rotateAll({vs[fs[i][1]][0], vs[fs[i][1]][1], vs[fs[i][1]][2]},
+                         rotations),
+        cv);
+    Vec3D cm_3 = camera.projectOnCanvas(
+        Vec3D::rotateAll({vs[fs[i][2]][0], vs[fs[i][2]][1], vs[fs[i][2]][2]},
+                         rotations),
+        cv);
 
-  //     LOG(cm_1)
+    Vec3D cv_1 = camera.projectOnCamera(Vec3D::rotateAll(
+        {vs[fs[i][0]][0], vs[fs[i][0]][1], vs[fs[i][0]][2]}, rotations));
+    Vec3D cv_2 = camera.projectOnCamera(Vec3D::rotateAll(
+        {vs[fs[i][1]][0], vs[fs[i][1]][1], vs[fs[i][1]][2]}, rotations));
+    Vec3D cv_3 = camera.projectOnCamera(Vec3D::rotateAll(
+        {vs[fs[i][2]][0], vs[fs[i][2]][1], vs[fs[i][2]][2]}, rotations));
 
-  //     // cv.drawTriangle(cm_1.x, cm_1.y, cm_2.x, cm_2.y, cm_3.x, cm_3.y,
-  //     //                 CONST_PICKER(0xff0000ff));
-  //     cv.drawLine(cm_1.x, cm_1.y, cm_2.x, cm_2.y, CONST_PICKER(0X3f0000ff));
-  //     cv.drawLine(cm_3.x, cm_3.y, cm_2.x, cm_2.y, CONST_PICKER(0X3f00ff00));
-  //     cv.drawLine(cm_3.x, cm_3.y, cm_1.x, cm_1.y, CONST_PICKER(0X3fff0000));
-  //   }
+    auto make_picker = [=](Vec3D a, Vec3D b, Vec3D c, int hue) {
+      return [=](int x, int y) -> uint32_t {
+        Vec3D cameraViewCoord = camera.canvas2CameraView(x, y, cv);
+        Vec3D normal = Vec3D::crossProduct(a - b, a - c);
+        float d = (a * normal) / (cameraViewCoord * normal);
+        Vec3D p = (d * cameraViewCoord) + camera.pos;
+
+        if (buf[y * W + x] > p.z) {
+          return 0xffff7f00;
+        }
+        return 0;
+      };
+    };
+
+    // cv.drawTriangle(cm_1.x, cm_1.y, cm_2.x, cm_2.y, cm_3.x, cm_3.y,
+    //                 make_picker(cv_1, cv_2, cv_3, 60));
+    // cv.drawLine(cm_1.x, cm_1.y, cm_2.x, cm_2.y, CONST_PICKER(0X3f0000ff));
+    // cv.drawLine(cm_3.x, cm_3.y, cm_2.x, cm_2.y, CONST_PICKER(0X3f00ff00));
+    // cv.drawLine(cm_3.x, cm_3.y, cm_1.x, cm_1.y, CONST_PICKER(0X3fff0000));
+  }
 }
 int main() { render(0); }
 COMMON_EXPORTS(W, H)
